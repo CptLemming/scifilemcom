938870b4976fbfe8adf7063859e3c80bafba1543
Ashley Wilson <ash@jp74.com>
2014-12-07 13:50:34 +0000

    Change production to sqlite

--- config/environments/production/settings_local.py
+++ config/environments/production/settings_local.py
@@ -4,14 +4,8 @@ THUMBNAIL_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE'    : 'django.db.backends.mysql',
-        'OPTIONS'   : {
-            'init_command': 'SET storage_engine=INNODB',
-        },
-        'NAME'      : 'databasename',
-        'USER'      : 'root',
-        'PASSWORD'  : '',
-        'HOST'      : '127.0.0.1',
-        'PORT'      : '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'scifilemcom.sqlite',
+        'USER': 'root'
     }
 }
